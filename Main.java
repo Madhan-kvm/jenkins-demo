caf432d075ceb9ee81613e5fbafe5242a928e87b
Madhan Mohan <your_github_email@gmail.com>
2026-08-10 17:19:29 +0530

    name modified

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, DevOps!");
+        System.out.println("Hello, madhan!");
         System.out.println("Jenkins CI is working!");
     }
 }
