213bd5038e89496abf47f4baefb2fd2c5e6599ac
Madhan Mohan <your_github_email@gmail.com>
2026-08-11 13:16:59 +0530

    another new change

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,6 +4,6 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println("Hello from Maven + Jenkins + Devops!");
-        System.out.println("My first CI/CD practice project.");
+        System.out.println("My first CI/CD practice project. !!");
     }
 }
